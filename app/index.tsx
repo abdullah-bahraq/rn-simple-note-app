aa9333f43520ec7e663cc87c03fb544b71003150
abdullah-bahraq <abdullah@bahraq.com>
2025-07-20 09:49:08 +0300

    chore: add Tailwind CSS configuration

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-red-700">
+      <Text className="text-2xl font-bold text-white">
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
